0c17597b28d54d93cf088e6c6bf2ba02a4548e23
MiguelABoni <miguelbedoyabonilla@gmail.com>
2022-06-30 10:30:38 -0500

    Proyecto primera versión Terminado

--- js/script.js
+++ js/script.js
@@ -7,8 +7,9 @@ let trainedNeuralNetwork;
 // Evento de click para realizar las respectivas predicciones a la red Neuronal
 document.getElementById('buttonTraining').addEventListener("click", () => {
     let inputTwitter = document.getElementById("inputTw").value;
-    console.log(inputTwitter);
-    console.log(neuronExecution(inputTwitter));
+    
+    document.getElementById("textResult").innerHTML = 'Según la predicción de la Red, la persona que publicó este Tweet es';
+    document.getElementById("result").innerHTML = neuronExecution(inputTwitter);
 });
 
 // Procesamiento cadenas de texto, conversión de cada caracter a código ASCII
@@ -26,6 +27,17 @@ const dataProcessing = (data) => {
     });
 }
 
+const trainingError = (callback) => {
+    let tbody = document.getElementById("tbody");
+    let newRow = document.createElement("tr");
+    let iterations = newRow.appendChild(document.createElement("td"));
+    let error = newRow.appendChild(document.createElement("td"));
+    iterations.innerHTML = callback.iterations;
+    error.innerHTML = callback.error;
+
+    tbody.appendChild(newRow);
+}
+
 // Entrenamiento de la Red Neuronal con el respectivo dataSet ingresado
 const neuralTraining = (dataSet) => {
     const neuron = new brain.NeuralNetwork();
@@ -34,8 +46,8 @@ const neuralTraining = (dataSet) => {
         activation: 'sigmoid',
         iterations: 20000,
         errorThresh: 0.005,
-        log: true,
-        logPeriod: 1,
+        log: trainingError,
+        logPeriod: 5,
         leakyReluAlpha: 0.01,
         learningRate: 0.3,
         momentum: 0.1,
@@ -47,13 +59,22 @@ const neuralTraining = (dataSet) => {
     });
 
     trainedNeuralNetwork = neuron.toFunction();
-
-    console.log('...Finished training...');
 }
 
 // Ingresamos y ejecutamos el Tweet que queremos predecir
 const neuronExecution = (tweet) => {
     let results = trainedNeuralNetwork(codify(tweet));
+    let imgResult = document.getElementById("imageResult");
+    let winResult;
+
+    if (results.Petro > results.Quintero) {
+        winResult = results.Petro;
+        imgResult.src = '../img/Petro.jpg';
+    } else {
+        winResult = results.Quintero;
+        imgResult.src = '../img/Quintero.jpg';
+    }
+    document.getElementById("porcentaje").innerHTML = (winResult*100).toFixed(3) + '%';
     
     return results.Petro > results.Quintero ? 'Petro' : 'Quintero';
 }
